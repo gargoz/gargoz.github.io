53bf7d97bc2e83cd3c06676db5c5280eb5a55f41
Cristhian Sánchez <81829087+gargoz@users.noreply.github.com>
2022-11-17 20:00:55 -0600

    Add files via upload

---
+++ PAGINA WEB JS/codigo hora.js
@@ -0,0 +1,39 @@
+/*let d = new Date();
+document.body.innerHTML = "<h1>Time right now is: " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
+"</h1>"*/
+
+
+/*function pruebaemail (valor){
+	re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+	if(!re.exec(valor)){
+		alert('email ERRÓNEO, inténtalo de nuevo');
+	}
+	else alert('email CORRECTO');
+	}
+
+---------------------------
+/*CREACIÓN DE VARIABLES:*/
+
+var producto="Refresco de 1 litro";
+
+var perro="raza bulldog";
+
+var gato="raza egipcia";
+
+var _perro="raza maltés";
+
+var perro_="raza x";
+
+perro_1="raza x";
+
+perro1="raza x";
+
+var Perro_chico;
+
+let perro_5="raza chihuaha";
+
+
+const perro_2="raza desconocida";
+
+	
+	console.log(perro_5 +", "+ perro_2);
